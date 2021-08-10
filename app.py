f30907438c1c40890d1f25401a67a5777b67a516
seyedalian <seyedalian@outlook.com>
2021-08-10 16:23:49 +0430

    save user to db , retrieve a user by email

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ from user import User
 
 my_user = User(email="seyedalian@outlook.com",first_name="Ali",last_name="Seyedalian",id = 1)
 
-print(my_user)
+new_user = User.load_from_db_by_email("seyedalian@outlook.com")
+
+print(new_user)
